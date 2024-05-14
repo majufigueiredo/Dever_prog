56d163800dcb6df91747d0a2d6ae3b533b217304
majufigueiredo <143432089+majufigueiredo@users.noreply.github.com>
2024-05-14 15:32:57 -0300

    Update GuessMariaFavoriteFood.py

--- GuessMariaFavoriteFood.py
+++ GuessMariaFavoriteFood.py
@@ -1,15 +1,15 @@
 def main():
-    print("Vamos jogar! Adivinhe qual é a comida favorita da Maria.")
-    print("A dica é: Começa com M e termina com U.")
+    st.write("Vamos jogar! Adivinhe qual é a comida favorita da Maria.")
+    st.write("A dica é: Começa com M e termina com U.")
     
     resposta_correta = "macarrão"  # A comida favorita da Maria
     
-    tentativa = input("Qual é a sua tentativa? ").lower()  # Converte a entrada para minúsculas
+    tentativa = st.text_input("Qual é a sua tentativa? ").lower()  # Converte a entrada para minúsculas
     
     if tentativa == resposta_correta:
-        print("Parabéns, você acertou!!")
+        st.write("Parabéns, você acertou!!")
     else:
-        print("Não foi dessa vez. Tente novamente!")
+        st.write("Não foi dessa vez. Tente novamente!")
 
 if __name__ == "__main__":
     main()
